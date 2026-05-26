67976d61701a384439e62b52eda764648104746e
miriyalavarshitha28-cmd <miriyalavarshitha28@gmail.com>
2026-05-26 10:00:11 +0530

    segregated frontend && backend

--- NOTES-application-main/backend/dist/tasks/dto/update-task.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateTaskDto = void 0;
-class UpdateTaskDto {
-    title;
-    description;
-    completed;
-}
-exports.UpdateTaskDto = UpdateTaskDto;
